7d3e2668081af94da80495c2432c64583870e95f
rekayno <92271828+rekayno@users.noreply.github.com>
2023-09-12 20:22:44 +0300

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -3,7 +3,7 @@ const express = require("express")
 const server = express()
 
 server.all("/", (req, res) => {
-  res.send("App is running!")
+  res.send("Nothing here ! (* ^ ω ^)")
 })
 
 function keepAlive() {
