1f21d49cdf2f76f49cdc471f5d87af7e7f78152a
rekayno <92271828+rekayno@users.noreply.github.com>
2024-01-09 19:21:17 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from threading import Thread
 
@@ -10,7 +11,7 @@ def home():
 
 
 def run():
-  app.run(host='0.0.0.0')
+  app.run(host='0.0.0.0', port=os.getenv('PORT') or 8080)
 
 
 def keep_alive():
